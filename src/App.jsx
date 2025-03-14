bebd3516848d31709cb2b311316c67e92290f25e
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2025-03-13 20:12:19 -0700

    basic style, font, image

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import '/assets/nith-placeholder.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Needle in the Hay</h1>
+			<h2>A short film teaser</h2>
+
+			<img
+			src="/assets/nith-placeholder.png"
+			alt="nith image"
+			/>
+
+{/*CALL TO ACTION*/}
+			<ul className="call_to_action">
+				<li>
+					<a 
+					href="https://seedandspark.com/fund/needle-in-the-hay#story">Support Us
+					</a>
+				</li>
+				<li>
+					<a
+					href="#">Share Us
+					</a>
+				</li>
+				<li>
+					<a
+					href="#">Join Us
+					</a>
+				</li>
+			</ul>
     </>
   )
 }
