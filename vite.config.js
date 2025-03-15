5dd98783a22eda6f3bdd38e3f65f57f631390323
Bill Bunkum <billbunkum@Billy-Boy-Uno.local>
2025-03-14 22:06:04 -0700

    change base to explicit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/needleinthehayfilm/',
   plugins: [react()],
+	base: 'https://billbunkum.github.io/needleinthehayfilm/',
 })
