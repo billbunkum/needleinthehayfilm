eac4ac6792c38f548dd8896850e1a4553f38e772
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2025-03-30 20:57:15 -0700

    change navigation

--- src/components/_navigation.jsx
+++ src/components/_navigation.jsx
@@ -1,23 +1,35 @@
 import '../App.css'
 
 function Navigation() {
+
+	function copyURL() {
+		const URL = "https://www.needleinthehayfilm.com";
+		navigator.clipboard.writeText(URL);
+		alert("Website URL copied: " + URL);
+	};
+
 	return (
 		<>
 {/*CALL TO ACTION*/}
 			<ul className="call_to_action">
 				<li>
-					<a 
-					href="https://seedandspark.com/fund/needle-in-the-hay#story">Support Us
+					<a
+						className="button"
+						href="https://seedandspark.com/fund/needle-in-the-hay?token=c0d2cac3031d07d83ccfde4abb6f78da050ad83b808c031bb538173e6b17f6f3#story">Support
 					</a>
 				</li>
 				<li>
 					<a
-					href="#">Share Us
+						className="button"
+						href="#"
+						onClick={copyURL}
+						>Share 
 					</a>
 				</li>
 				<li>
 					<a
-					href="#">Join Us
+						className="button"
+						href="https://needleinthehayfilm.substack.com">Newsletter
 					</a>
 				</li>
 			</ul>
