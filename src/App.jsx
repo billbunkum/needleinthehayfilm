d5fd10cabecc5f8ce18495437b0015e14167211c
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2025-04-09 14:34:56 -0700

    add trailer and resize

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,20 @@ function App() {
 			<Navigation />
 
 			<div className="splash_section">
-				<img
+				<iframe 
+					className="trailer"
+					src="https://www.youtube.com/embed/uBoDZep86gk?si=LblMA4OGxPXhaNM5" 
+					title="YouTube video player" 
+					frameborder="0" 
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+					referrerpolicy="strict-origin-when-cross-origin" 
+					allowfullscreen>
+				</iframe>
+{/*				<img
 				src="assets/nith-placeholder.png"
 					alt="nith image"
 				/>
+*/}
 			</div>
 
 			<Teaser />
