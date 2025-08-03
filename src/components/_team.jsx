d7807cf95fa3fab990611561bd950af9f940d690
Bill Bunkum <billbunkum@Billy-Boy-Uno.local>
2025-08-02 21:27:55 -0700

    add Diego Ramos bio and socials; add socials to William Hurst

--- src/components/_team.jsx
+++ src/components/_team.jsx
@@ -16,6 +16,30 @@ Lucy Page is a Los Angeles-based actress and first-time director, known for her 
 					</li>
 
 					<li><h2>PRODUCER – William Hurst</h2></li>
+					<div className="socials">
+						<a href="https://www.instagram.com/@needleinthehayfilm">
+							<img
+								className="social_icon_ig social_icons"
+								src="assets/images/socials/instagram.png"
+								alt="IG"
+							/>
+						</a>
+						<a href="https://www.imdb.com/name/nm12293482/">
+							<img
+								className="social_icon_imdb social_icons"
+								src="assets/images/socials/imdb-logo.png"
+								alt="IMDb"
+							/>
+						</a>
+						<a href="https://needleinthehayfilm.substack.com/">
+							<img
+								className="social_icon_comedy_mask social_icons"
+								src="assets/images/icons/comedy-mask-icon.png"
+								alt="Project"
+							/>
+						</a>
+					</div>
+
 					<li><img
 						className="headshots"
 						src="assets/images/headshots/william-hurst-producer-headshot.jpg"
@@ -93,13 +117,36 @@ Adelaide Johnson is a skilled First Assistant Director based in Los Angeles. Wit
 					</li>
 
 					<li><h2>GRIP - Diego Ramos</h2></li>
+					<div className="socials">
+						<a href="https://www.instagram.com/diegoramos248/?hl=en">
+							<img
+								className="social_icon_ig social_icons"
+								src="assets/images/socials/instagram.png"
+								alt="IG"
+							/>
+						</a>
+						<a href="https://www.imdb.com/name/nm11431393/?ref_=tt_ov_1_1">
+							<img
+								className="social_icon_imdb social_icons"
+								src="assets/images/socials/imdb-logo.png"
+								alt="IMDb"
+							/>
+						</a>
+						<a href="https://www.dropbox.com/scl/fi/mql9e7inptw5ribqzt0rr/Rock-of-the-Dead.mp4?rlkey=wxvh196b4g6ifd3c93mvi3r1f&st=gmwa4dta&dl=0">
+							<img
+								className="social_icon_comedy_mask social_icons"
+								src="assets/images/icons/comedy-mask-icon.png"
+								alt="Project"
+							/>
+						</a>
+					</div>
 					<li><img
 						className="headshots"
-						src="assets/images/icons/contacts.png"
+						src="assets/images/headshots/diego-ramos-headshot.jpg"
 						alt="Diego Ramos"
 						/>
 						<p>
-...more about Diego here!
+Diego Ramos is a Mexican director, screenwriter, playwright, and musician. He was born on July 8th, 1998. His love and passion for film began at a very young age when he saw Jurassic Park for the first time; the special effects blew him away, and he just knew he had to be part of that world. He began writing short stories as a child and continues to write stories to this very day.
 						</p>
 					</li>
 
@@ -193,6 +240,30 @@ Kelly Meyersfield is an actress, singer, writer, and founder of the Professional
 					</li>
 
 					<li><h2>William Hurst as BRANDON MOORE</h2></li>
+					<div className="socials">
+						<a href="https://www.instagram.com/@william_r_hurst">
+							<img
+								className="social_icon_ig social_icons"
+								src="assets/images/socials/instagram.png"
+								alt="IG"
+							/>
+						</a>
+						<a href="https://www.imdb.com/name/nm12293482/">
+							<img
+								className="social_icon_imdb social_icons"
+								src="assets/images/socials/imdb-logo.png"
+								alt="IMDb"
+							/>
+						</a>
+						<a href="https://needleinthehayfilm.substack.com/">
+							<img
+								className="social_icon_comedy_mask social_icons"
+								src="assets/images/icons/comedy-mask-icon.png"
+								alt="Project"
+							/>
+						</a>
+					</div>
+
 					<li><img
 						className="headshots"
 						src="assets/images/headshots/william-hurst-headshot.jpg"
