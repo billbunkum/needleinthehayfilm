cb054fd1062cad90b4538779cde8c2edaf5f68ba
billbunkum <20425559+billbunkum@users.noreply.github.com>
2026-07-03 19:39:32 -0700

    update logline; trailer link

--- src/components/_teaser.jsx
+++ src/components/_teaser.jsx
@@ -3,11 +3,7 @@ function Teaser() {
 	return (
 		<>
 			<div>
-"In a gritty, dimly lit apartment, a washed-up musician teeters on the edge of self-destruction, haunted by his past and crushed by his present.
-
-When his fed-up landlord storms in demanding overdue rent his loyal but exasperated Music Producer has to decide whether to get involved or not.
-
-Needle in the Hay is a raw, tension-filled drama about broken dreams, fractured relationships, and the fragile hope for redemption."	
+"When a struggling musician disappears into addiction and depression, his manager is forced to confront the limits of loyalty, friendship, and responsibility."
 			</div>
 		</>
 	)
--- src/components/_trailer.jsx
+++ src/components/_trailer.jsx
@@ -5,7 +5,7 @@ function Trailer() {
 			<div className="splash_section">
 				<iframe 
 					className="trailer"
-					src="https://www.youtube.com/embed/uBoDZep86gk?si=LblMA4OGxPXhaNM5" 
+					src="https://www.youtube.com/embed/UL9YT3xqgcA?si=SaRRpJ0aZ1LdXx2W" 
 					title="YouTube video player" 
 					frameborder="0" 
 					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
